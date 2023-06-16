0056a7bcb980a4827d03daa498b1103a7247bb45
Teye Odonkor <odonkorteye@yahoo.com>
2023-06-16 10:43:23 -0700

    program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,11 @@ int main(void)
 		{
 			putchar(a);
 			putchar(b);
-			putchar(',');
-			putchar(' ');
+			if (a != '8' || b != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
